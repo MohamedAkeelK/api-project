a15ff3c8a35932b9033bf2a7720e16216f979d46
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-10-31 10:52:49 -0400

    added monoogseconfig

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
+
 const URL =
   process.env.MONGO_URL || "mongodb://127.0.0.1:27017/social-media-users";
 
+let mongooseConfig = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
 mongoose.set("returnOriginal", false);
 
-mongoose.connect(URL).catch((err) => {
+mongoose.connect(URL, mongooseConfig).catch((err) => {
   console.log(`Error connection go MongoDB: ${err.message}`);
 });
 
